2521e2eddf9b140d74683443361c9003100dfacf
nguyenducquyen <nguyenducquyen2k1@gmail.com>
2024-01-20 21:48:47 +0700

    update url mongoDB

--- config/db_connect.js
+++ config/db_connect.js
@@ -1,9 +1,9 @@
 // const { default: mongoose } = require("mongoose");
 const mongoose = require("mongoose");
-
+const uri = `mongodb+srv://nguyenducquyen2k1:gyEpCAoozIPYN4Ih@cluster0.ag8s8xs.mongodb.net/ecommerce?retryWrites=true&w=majority`;
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+    const conn = await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -13,8 +13,9 @@ const dbConnect = async () => {
       console.log("Db connect error");
     }
   } catch (error) {
-    console.log("Db connect error ", error);
-    throw new Error(error);
+    // console.log("Db connect error ", error);
+    console.error("Db connect error", error);
+    // throw new Error(error);
   }
 };
 module.exports = dbConnect;
